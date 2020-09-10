2310b2a7245c8f2f214564e594b4ef985420786b
Anton Borisov <lerdenfart@gmail.com>
2020-09-10 19:52:24 +0300

    hometask_2 add some *

--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -11,6 +11,6 @@ class TextMessage(
     date: Date = Date(),
     var text:String?
 ):BaseMessage(id,from,chat,isIncoming,date) {
-    override fun formatMessage(): String =//"id:$id ${from?.lastName} : " +
-            "${if(isIncoming) "получил" else "отправил"} сообщение: \"$text\" ${date.humanizeDiff()}"
+    override fun formatMessage(): String ="${from?.firstName} " +  //id:$id
+            "${if(isIncoming) "получил" else "отправил"} сообщение \"$text\" ${date.humanizeDiff()}"
 }
